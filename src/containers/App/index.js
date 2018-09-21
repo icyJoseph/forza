ced41b2e2f03d8b923599eb01bee41cf6a701caa
icyJoseph <sephxd1234@gmail.com>
2018-09-21 18:37:50 +0200

    style: show borders on components to analyze layout options

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,15 +1,21 @@
-import React from "react";
-import styled from "styled-components";
-import TopScorer from "../TopScorer";
+import React, { Fragment } from "react";
+import styled, { css } from "styled-components";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 import List from "../../components/List";
 import Podium from "../../components/Podium";
+import TopScorer from "../TopScorer";
+
 import Mock from "../../mock/api";
 import { buildPlayersTree } from "../../helpers";
 const breakpoint = "(min-width: 599px)";
 
 const { leagues } = Mock;
-const { teams } = leagues[0];
+const { teams, leagueName, country } = leagues[0];
 
 const players = buildPlayersTree(teams);
 
@@ -26,21 +32,46 @@ const Stick = styled.div`
   background-color: white;
 `;
 
+const baseStyle = css`
+  flex-grow: 1;
+`;
+
+const MainTitle = styled(Typography)`
+  ${baseStyle};
+`;
+
+const Bar = styled(AppBar)`
+  ${baseStyle};
+  border: 1px solid;
+`;
+
 const App = () => (
-  <div>
+  <Fragment>
     <Stick>
-      <header>
-        <h1>Your Premier League Predictions</h1>
-      </header>
-      <Podium query={breakpoint} predictions={predictions} />
+      <Bar position="static" color="primary">
+        <Toolbar>
+          <MainTitle variant="title" color="inherit">
+            {leagueName} Predictions - {country}
+          </MainTitle>
+          <Button color="inherit">Share</Button>
+          <Button color="inherit">Reset</Button>
+        </Toolbar>
+      </Bar>
+      <div style={{ marginTop: 40, border: "1px solid" }}>
+        <Podium query={breakpoint} predictions={predictions} />
+      </div>
     </Stick>
-    <TopScorer>
-      {Object.keys(players).map(player => (
-        <div key={player}>{player}</div>
-      ))}
-    </TopScorer>
-    <List teams={teams} callback={e => console.log(e)} />
-  </div>
+    <div style={{ marginTop: 40, border: "1px solid" }}>
+      <TopScorer>
+        {Object.keys(players).map(player => (
+          <div key={player}>{player}</div>
+        ))}
+      </TopScorer>
+    </div>
+    <div style={{ marginTop: 40, border: "1px solid" }}>
+      <List teams={teams} />
+    </div>
+  </Fragment>
 );
 
 export default App;
