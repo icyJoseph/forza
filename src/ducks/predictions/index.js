350bcf6745be21fbbf3a310bdc118b82cc40f32b
icyJoseph <sephxd1234@gmail.com>
2018-09-22 12:56:55 +0200

    feat: handle multiple predictions

--- src/ducks/predictions/index.js
+++ src/ducks/predictions/index.js
@@ -4,7 +4,7 @@ const SET_TOPSCORER = "prediction top scorer";
 //actions
 export const setPrediction = (team, place) => ({
   type: SET_PREDICTION,
-  payload: { team: team.teamName, place }
+  payload: { ...team, place }
 });
 
 export const setTopScorer = player => ({
@@ -14,22 +14,30 @@ export const setTopScorer = player => ({
 
 //initial state
 const initialState = {
-  predictions: [],
+  predictions: {},
   topScorer: ""
 };
 
 //reducer
 function predictions(state = initialState, action) {
+  const { league } = action.payload;
   switch (action.type) {
     case SET_PREDICTION:
+      const current = state.predictions[league] || [];
       return {
         ...state,
-        predictions: state.predictions.concat(action.payload)
+        predictions: {
+          ...state.predictions,
+          [league]: current.concat({
+            ...action.payload,
+            league
+          })
+        }
       };
     case SET_TOPSCORER:
       return {
         ...state,
-        topScorer: action.payload.playerName
+        topScorer: { ...state.topScorer, [league]: action.payload.playerName }
       };
   }
 }
--- src/ducks/predictions/test/predictions.test.js
+++ src/ducks/predictions/test/predictions.test.js
@@ -2,18 +2,20 @@ import predictions, { setPrediction, setTopScorer } from "../";
 
 describe("it handles user predictions", () => {
   it("takes a teams place prediction", () => {
-    expect(predictions(undefined, setPrediction({ teamName: "A" }, 1))).toEqual(
-      {
-        predictions: [{ team: "A", place: 1 }],
-        topScorer: ""
-      }
-    );
+    expect(
+      predictions(undefined, setPrediction({ league: "La", teamName: "A" }, 1))
+    ).toEqual({
+      predictions: { La: [{ teamName: "A", place: 1, league: "La" }] },
+      topScorer: ""
+    });
   });
 
   it("takes top scorer prediction", () => {
-    expect(predictions(undefined, setTopScorer({ playerName: "JS" }))).toEqual({
-      predictions: [],
-      topScorer: "JS"
+    expect(
+      predictions(undefined, setTopScorer({ playerName: "JS", league: "La" }))
+    ).toEqual({
+      predictions: {},
+      topScorer: { La: "JS" }
     });
   });
 });
