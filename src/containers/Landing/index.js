f69ef7b41179688b627ece9c712950547dd49e3e
icyJoseph <sephxd1234@gmail.com>
2018-09-23 13:08:44 +0200

    feat: landing page

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -1,37 +1,58 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-
+import styled from "styled-components";
+import Typography from "@material-ui/core/Typography";
+import Card, { CardContainer } from "../../components/Card";
 import { mapAllLeaguesToProps, mapFetchAction } from "../../ducks/leagues";
+import { curry } from "../../helpers";
+import logo from "../../logo.png";
+
+const HeaderSegment = styled.div`
+  margin: 20px auto 0;
+  text-align: center;
+`;
+
+const CustomHeader = (Message, variant) => (
+  <HeaderSegment>
+    <Typography variant={variant} gutterBottom>
+      {Message}
+    </Typography>
+  </HeaderSegment>
+);
+
+const ImageHeader = logo => (
+  <HeaderSegment>
+    <img src={logo} alt="Predictions!" />
+  </HeaderSegment>
+);
 
 export class Landing extends Component {
   componentDidMount() {
     this.props.fetch();
   }
 
-  goToLeague = e => {
-    const encoded = encodeURI(e.target.id);
+  goToLeague = id => {
+    const encoded = encodeURI(id);
     return this.props.history.push(`/${encoded}`);
   };
 
   render() {
     const { allLeagues } = this.props;
     return (
-      <div>
-        <div style={{ margin: 50 }}>
-          <div>Hi! Welcome to Predictions App</div>
-          <div>Which league would you like to predict?</div>
-        </div>
-        {allLeagues.map(league => (
-          <div
-            style={{ margin: 50 }}
-            id={league.leagueId}
-            key={league.leagueId}
-            onClick={this.goToLeague}
-          >
-            {league.leagueName}
-          </div>
-        ))}
-      </div>
+      <Fragment>
+        {CustomHeader("Hello!", "display3")}
+        {ImageHeader(logo)}
+        {CustomHeader(" Which league would you like to predict?", "subheading")}
+        <CardContainer>
+          {allLeagues.map(({ leagueId, leagueName, country, teams }) => (
+            <Card key={leagueId} handler={curry(this.goToLeague)(leagueId)}>
+              {leagueName}
+              {country}
+              {`${teams.length} Teams`}
+            </Card>
+          ))}
+        </CardContainer>
+      </Fragment>
     );
   }
 }
