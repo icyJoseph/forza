8f3f7c9ddaab2383f50fb4533d788a6c5c9e3e97
icyJoseph <sephxd1234@gmail.com>
2018-09-23 19:17:44 +0200

    feat: render multiple items, either players or teams

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,73 +1,47 @@
-import React, { Component } from "react";
-import Collapse from "@material-ui/core/Collapse";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import React from "react";
+import styled from "styled-components";
+import Card from "../Card";
 
-const ListContainer = ({ teams, callback }) => (
-  <div
-    style={{
-      display: "flex",
-      flexWrap: "wrap"
-    }}
-  >
-    {teams.map(team => (
-      <ListElement key={team.teamId} {...team} callback={callback} />
+const Flag = styled.div.attrs({
+  style: ({ color }) => ({
+    backgroundColor: `${color}`,
+    border: color === "#FFFFFF" ? "1px dashed gray" : ""
+  })
+})`
+  width: 30px;
+  height: 5px;
+  margin: 3px auto;
+`;
+
+export const Team = ({ teamName, teamColor, topPlayers, callback }) => (
+  <Card handler={callback}>
+    {teamName}
+    <Flag color={teamColor} />
+    {topPlayers.map(({ playerName }) => (
+      <div key={playerName}>{playerName}</div>
     ))}
-  </div>
+  </Card>
 );
 
-class ListElement extends Component {
-  state = {
-    open: false,
-    selection: false
-  };
+export const Player = ({
+  playerName,
+  teamName,
+  teamColor,
+  goalsLastSeason,
+  callback
+}) => (
+  <Card handler={callback}>
+    {playerName}
+    {goalsLastSeason}
+    {teamName}
+    <Flag color={teamColor} />
+  </Card>
+);
 
-  toggle = () => this.setState(prev => ({ open: !prev.open }));
-
-  render() {
-    const { open, selection } = this.state;
-    const { teamName, callback } = this.props;
-    return (
-      <div
-        style={{
-          width: "200px",
-          margin: "10px auto 10px",
-          border: "1px solid gray",
-          borderRadius: "20px"
-        }}
-      >
-        <div
-          style={{ textAlign: "center" }}
-          onClick={() => callback({ name: teamName })}
-        >
-          {teamName}
-        </div>
-        <IconButton aria-label="Show more" onClick={this.toggle}>
-          <ExpandMoreIcon />
-        </IconButton>
-        <Collapse in={open}>{teamName}</Collapse>
-        {selection &&
-          [1, 2, 3].map(place => (
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <Button
-                variant="fab"
-                mini
-                style={{
-                  position: "relative",
-                  background: "gray",
-                  left: "180px",
-                  bottom: "20px",
-                  color: "white"
-                }}
-              >
-                {place}
-              </Button>
-            </div>
-          ))}
-      </div>
-    );
-  }
-}
+export const ListContainer = ({ items, callback, type = "team" }) =>
+  items.map((item, index) => {
+    const Component = type === "players" ? Player : Team;
+    return <Component key={`${type}-${index}`} {...item} callback={callback} />;
+  });
 
 export default ListContainer;
