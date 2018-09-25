024778b1444ac8c645b9b41e938683ac6be4bfd6
icyJoseph <sephxd1234@gmail.com>
2018-09-25 10:14:55 +0200

    style: create MUI theme provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Routes from "./routes";
 import registerServiceWorker from "./registerServiceWorker";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 import { createStore } from "redux";
 import { Provider } from "react-redux";
@@ -12,11 +13,22 @@ const reduxDevTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const store = createStore(rootReducer, undefined, reduxDevTools);
+const dodgerBlue = "#1E90FF";
+const white = "#ffffff";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: dodgerBlue },
+    secondary: { main: white }
+  }
+});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById("root")
 );
 registerServiceWorker();
