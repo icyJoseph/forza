8c28d8fdee8652bd161a51516c1ee93111fa489d
icyJoseph <sephxd1234@gmail.com>
2018-09-26 08:03:46 +0200

    feat: :sparkles: add emoji component

---
+++ src/components/Emoji/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const labelSymbolSelector = place => {
+  switch (place) {
+    case 1:
+      return { symbol: "🥇", label: "1st" };
+    case 2:
+      return { symbol: "🥈", label: "2nd" };
+    case 3:
+      return { symbol: "🥉", label: "3rd" };
+    case "ball":
+      return { symbol: "⚽️", label: "Best" };
+    default:
+      return { symbol: "", label: "" };
+  }
+};
+
+export const Emoji = ({ place }) => {
+  const { label, symbol } = labelSymbolSelector(place);
+  return (
+    <span
+      className="emoji"
+      role="img"
+      aria-label={label ? label : ""}
+      aria-hidden={label ? "false" : "true"}
+      style={{ fontSize: "16pt" }}
+    >
+      {symbol}
+    </span>
+  );
+};
+export default Emoji;
