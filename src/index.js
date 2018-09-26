9b67f8c12ffdbe4a335fdb4b5237830a0f2ebd57
icyJoseph <sephxd1234@gmail.com>
2018-09-26 10:10:00 +0200

    feat: apply caching enhancer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Routes from "./routes";
-import registerServiceWorker from "./registerServiceWorker";
+import { createStore, compose } from "redux";
+import { Provider } from "react-redux";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-import { createStore } from "redux";
-import { Provider } from "react-redux";
+import Routes from "./routes";
+import saveToStore from "./middleware";
 import rootReducer from "./ducks";
 
-const reduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+import { STATE_NAME, white, dodgerBlue } from "./contants";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
 
-const store = createStore(rootReducer, undefined, reduxDevTools);
-const dodgerBlue = "#1E90FF";
-const white = "#ffffff";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+  : compose;
+
+// redux dev tools plus saving to local storage
+const enhancer = composeEnhancers(saveToStore());
+
+// get forza key from local storage
+const storedStateJSON = localStorage.getItem(STATE_NAME);
+
+// parse it or returnning undefined
+const storedInitialState = storedStateJSON
+  ? JSON.parse(storedStateJSON)
+  : undefined;
+
+const store = createStore(rootReducer, storedInitialState, enhancer);
 
 const theme = createMuiTheme({
   palette: {
