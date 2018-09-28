be0beebb533ddd8cfbadbf44f91fb56c93b5dc78
icyJoseph <sephxd1234@gmail.com>
2018-09-28 09:15:36 +0200

    fix: use delay blocker inside Loading component

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,18 +1,19 @@
 import React from "react";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-export const Loading = () => (
-  <div
-    style={{
-      width: "100%",
-      display: "flex",
-      justifyContent: "center",
-      top: "50%",
-      position: "sticky"
-    }}
-  >
-    <CircularProgress />
-  </div>
-);
+export const Loading = ({ pastDelay }) =>
+  pastDelay && (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        top: "50%",
+        position: "sticky"
+      }}
+    >
+      <CircularProgress />
+    </div>
+  );
 
 export default Loading;
